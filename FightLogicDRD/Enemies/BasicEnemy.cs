6c262c51bf315bbabf6f15effebc50d43a88c5b7
Richard Dubný <richarddubny18@gmail.com>
2021-11-22 00:20:08 +0100

    Revert "Revert "adding basic enemies""
    This reverts commit 00b14109a819027250d36f6ed47d80d1ea5dae7a.

---
+++ FightLogicDRD/Enemies/BasicEnemy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FightLogicDRD.Enemies
+{
+    class BasicEnemy
+    {
+        public int sila;
+        public int zivot;
+        public int obrana;
+
+        public void Utoc()
+        {
+
+        }
+        public void BranSe()
+        {
+
+        }
+        public void Umri()
+        {
+            Console.WriteLine("BasicEnemy umira");
+        }
+    }
+}
---
+++ FightLogicDRD/Enemies/Mage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FightLogicDRD.Enemies
+{
+    class Mage
+    {
+        public int mana;
+        public int zivoty;
+
+        public void FireBall()
+        {
+            mana--;
+        }
+        public void LightingAttack() {
+            mana--;
+        }
+        public void FireStorm()
+        {
+            mana - 10;
+        }
+    }
+}
